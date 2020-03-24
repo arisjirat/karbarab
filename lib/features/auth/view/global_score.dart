8cb8ae4785abb7f19cd8ed3d2146ca45a270d316
Aris Jirat <arisjiratkurniawan@gmail.com>
2020-03-25 01:39:17 +0700

    fix linter

--- lib/features/auth/view/global_score.dart
+++ lib/features/auth/view/global_score.dart
@@ -235,7 +235,7 @@ class _GlobalScoreState extends State<GlobalScore> {
               },
             );
           }
-          return SpinKitRotatingPlain(color: greenColor);
+          return const SpinKitRotatingPlain(color: greenColor);
         },
       ),
     );
