25317ae4127e62ebf1e2907f68d287d7735d410e
Aris Jirat <arisjiratkurniawan@gmail.com>
2020-04-23 23:57:04 +0700

    listen is user null

--- lib/features/battle_list/view/battle_list_view.dart
+++ lib/features/battle_list/view/battle_list_view.dart
@@ -25,48 +25,65 @@ class _BattleListViewState extends State<BattleListView> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 10),
-      child:
-          BlocBuilder<BattleListBloc, BattleListState>(builder: (ctx, state) {
-        if (state is HasBattleList) {
-          if (state.isLoading) {
-            return const SpinKitDoubleBounce(color: greenColor);
+      child: BlocListener<BattleListBloc, BattleListState>(
+        listener: (ctx, state) {
+          // force request user null
+          if (state is HasBattleList &&
+              state.quizBattle.isNotEmpty &&
+              state.user == null) {
+            Future.delayed(const Duration(milliseconds: 100), () {
+              BlocProvider.of<BattleListBloc>(context).add(GetBattleList());
+            });
           }
-          if (state.quizBattle.isNotEmpty) {
-            return ListView.builder(
-              padding: const EdgeInsets.all(0),
-              itemCount: state.quizBattle.length,
-              itemBuilder: (context, position) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 10.0),
-                  child: CardBattleItem(
-                    user: state.user,
-                    onAnswer: () {
-                      Navigator.push(
-                        ctx,
-                        MaterialPageRoute(
-                          builder: (ct) => BattleAnswerScreen(battle: state.quizBattle[position],),
-                        ),
-                      );
-                    },
-                    key: Key(state.quizBattle[position].scoreId),
-                    score: state.quizBattle[position],
-                  ),
+        },
+        child: BlocBuilder<BattleListBloc, BattleListState>(
+          builder: (ctx, state) {
+            if (state is HasBattleList) {
+              if (state.isLoading) {
+                return const SpinKitDoubleBounce(color: greenColor);
+              }
+              if (state.quizBattle.isNotEmpty && state.user != null) {
+                return ListView.builder(
+                  padding: const EdgeInsets.all(0),
+                  itemCount: state.quizBattle.length,
+                  itemBuilder: (context, position) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 10.0),
+                      child: CardBattleItem(
+                        user: state.user,
+                        onAnswer: () {
+                          Navigator.push(
+                            ctx,
+                            MaterialPageRoute(
+                              builder: (ct) => BattleAnswerScreen(
+                                battle: state.quizBattle[position],
+                              ),
+                            ),
+                          );
+                        },
+                        key: Key(state.quizBattle[position].scoreId),
+                        score: state.quizBattle[position],
+                      ),
+                    );
+                  },
                 );
-              },
-            );
-          } else if (state.quizBattle.isEmpty) {
-            return Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                RegularText(text: 'Tidak ada kartu kiriman',)
-              ],
-            );
-          }
-        }
-        return const SpinKitDoubleBounce(color: greenColor);
-      }),
+              } else if (state.quizBattle.isEmpty) {
+                return Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    RegularText(
+                      text: 'Tidak ada kartu kiriman',
+                    )
+                  ],
+                );
+              }
+            }
+            return const SpinKitDoubleBounce(color: greenColor);
+          },
+        ),
+      ),
     );
   }
 }
