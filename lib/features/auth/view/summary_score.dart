e472886786914713f09a1da8c53ab3755bca11e4
Aris Jirat <arisjiratkurniawan@gmail.com>
2020-04-23 17:20:14 +0700

    switch good quiz, bad quiz

--- lib/features/auth/view/summary_score.dart
+++ lib/features/auth/view/summary_score.dart
@@ -57,63 +57,6 @@ class _SummaryScoreState extends State<SummaryScore>
                       return TabBarView(
                         controller: _tabController,
                         children: <Widget>[
-                          state.badQuiz.isEmpty
-                              ? Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    SmallerText(
-                                      dark: true,
-                                      text:
-                                          'Belum ada quiz yang buruk untuk kamu',
-                                    ),
-                                    RaisedButton(
-                                      shape: const RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.all(
-                                          Radius.circular(10),
-                                        ),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.of(context)
-                                            .pushNamed(HomeScreen.routeName);
-                                      },
-                                      color: secondaryColor,
-                                      child: Row(
-                                        mainAxisSize: MainAxisSize.min,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: SmallerText(
-                                              text: 'Main',
-                                              dark: true,
-                                            ),
-                                          ),
-                                          Icon(Icons.videogame_asset,
-                                              color: textColor),
-                                        ],
-                                      ),
-                                      elevation: 5,
-                                    ),
-                                  ],
-                                )
-                              : ListView.builder(
-                                  itemCount: state.badQuiz.length,
-                                  itemBuilder: (context, position) {
-                                    return CardItem(
-                                      key: Key(state.badQuiz[position].quizId),
-                                      positive: false,
-                                      quiz:
-                                          state.badQuiz[position].quiz,
-                                      totalScore:
-                                          state.badQuiz[position].totalScore,
-                                      averageScore:
-                                          state.badQuiz[position].averageScore,
-                                      gameMode:
-                                          state.badQuiz[position].quizMode,
-                                    );
-                                  },
-                                ),
                           state.goodQuiz.isEmpty
                               ? Column(
                                   mainAxisAlignment: MainAxisAlignment.center,
@@ -121,6 +64,10 @@ class _SummaryScoreState extends State<SummaryScore>
                                     SmallerText(
                                       dark: true,
                                       text: 'Belum ada quiz terbaik untuk kamu',
+                                    ),
+                                    SmallerText(
+                                      dark: true,
+                                      text: 'Nilai rata rata kartu di atas 75',
                                     ),
                                     RaisedButton(
                                       shape: const RoundedRectangleBorder(
@@ -159,14 +106,73 @@ class _SummaryScoreState extends State<SummaryScore>
                                     return CardItem(
                                       key: Key(state.goodQuiz[position].quizId),
                                       positive: true,
-                                      quiz:
-                                          state.goodQuiz[position].quiz,
+                                      quiz: state.goodQuiz[position].quiz,
                                       totalScore:
                                           state.goodQuiz[position].totalScore,
                                       averageScore:
                                           state.goodQuiz[position].averageScore,
                                       gameMode:
                                           state.goodQuiz[position].quizMode,
+                                    );
+                                  },
+                                ),
+                          state.badQuiz.isEmpty
+                              ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    SmallerText(
+                                      dark: true,
+                                      text:
+                                          'Belum ada quiz yang buruk untuk kamu',
+                                    ),
+                                    SmallerText(
+                                      dark: true,
+                                      text: 'Nilai rata rata kartu 75 ke bawah',
+                                    ),
+                                    RaisedButton(
+                                      shape: const RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(10),
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.of(context)
+                                            .pushNamed(HomeScreen.routeName);
+                                      },
+                                      color: secondaryColor,
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: SmallerText(
+                                              text: 'Main',
+                                              dark: true,
+                                            ),
+                                          ),
+                                          Icon(Icons.videogame_asset,
+                                              color: textColor),
+                                        ],
+                                      ),
+                                      elevation: 5,
+                                    ),
+                                  ],
+                                )
+                              : ListView.builder(
+                                  itemCount: state.badQuiz.length,
+                                  itemBuilder: (context, position) {
+                                    return CardItem(
+                                      key: Key(state.badQuiz[position].quizId),
+                                      positive: false,
+                                      quiz: state.badQuiz[position].quiz,
+                                      totalScore:
+                                          state.badQuiz[position].totalScore,
+                                      averageScore:
+                                          state.badQuiz[position].averageScore,
+                                      gameMode:
+                                          state.badQuiz[position].quizMode,
                                     );
                                   },
                                 ),
@@ -207,16 +213,16 @@ class _SummaryScoreState extends State<SummaryScore>
               tabs: <Widget>[
                 Tab(
                   icon: Icon(
-                    Icons.mood_bad,
+                    Icons.star_border,
                     color: _tabController.index == 0 ? whiteColor : greenColor,
                   ),
                 ),
                 Tab(
                   icon: Icon(
-                    Icons.star_border,
+                    Icons.mood_bad,
                     color: _tabController.index == 1 ? whiteColor : greenColor,
                   ),
-                )
+                ),
               ],
             ),
           ),
