9287047315d4c0dff929ad8cde047bd87f88a51d
Aris Jirat <arisjiratkurniawan@gmail.com>
2020-04-23 23:45:07 +0700

    re request user null

--- lib/features/auth/view/global_score.dart
+++ lib/features/auth/view/global_score.dart
@@ -8,6 +8,9 @@ import 'package:karbarab/features/admob/bloc/admob_bloc.dart';
 import 'package:karbarab/features/admob/view/ads.dart';
 import 'package:karbarab/features/auth/view/card_score_item.dart';
 import 'package:karbarab/features/global_scores/bloc/global_scores_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const LAST_TIME_WATCH_ADS = 'last_time_watch_ads';
 
 class GlobalScore extends StatefulWidget {
   @override
@@ -15,25 +18,54 @@ class GlobalScore extends StatefulWidget {
 }
 
 class _GlobalScoreState extends State<GlobalScore> {
-  bool _watched = false;
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  void _handleReward() {
+  bool _watched = false;
+  bool _checking = false;
+
+  void _handleReward() async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setInt(LAST_TIME_WATCH_ADS,
+        DateTime.now().add(const Duration(minutes: 15)).millisecondsSinceEpoch);
     setState(() {
       _watched = true;
     });
+  }
+
+  void _isNeedToWatchAgain() async {
+    final SharedPreferences prefs = await _prefs;
+    final int lastWatch = prefs.getInt(LAST_TIME_WATCH_ADS);
+    if (DateTime.fromMillisecondsSinceEpoch(lastWatch)
+        .isAfter(DateTime.now())) {
+      print('is after');
+      setState(() {
+        _watched = true;
+        _checking = false;
+      });
+    } else {
+      setState(() {
+        _watched = false;
+        _checking = false;
+      });
+    }
   }
 
   @override
   void initState() {
     super.initState();
     BlocProvider.of<GlobalScoresBloc>(context).add(GetGlobalScores());
+    _isNeedToWatchAgain();
+    setState(() {
+      _checking = true;
+    });
   }
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
+    _isNeedToWatchAgain();
     setState(() {
-      _watched = false;
+      _checking = true;
     });
   }
 
@@ -57,51 +89,57 @@ class _GlobalScoreState extends State<GlobalScore> {
                     height: deviceHeight(context) / 5,
                   ),
                 ),
-                Align(
-                  alignment: Alignment.center,
-                  child: RegularText(
-                    text: 'Mau lihat score global?',
-                    dark: true,
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: RegularText(
-                    text: 'Tonton iklan dulu yuk',
-                    dark: true,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                AdsScreen(
-                  adsMode: AdsMode.GlOBAL_SCORE,
-                  onReward: _handleReward,
-                  buttonShow: Container(
-                    padding: const EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: greenColor,
-                      boxShadow: [
-                        BoxShadow(
-                          color: textColor.withOpacity(0.5),
-                          offset: const Offset(0.0, 0.0),
-                        ),
-                        BoxShadow(
-                          color: textColor.withOpacity(0.5),
-                          offset: const Offset(0.0, 0.1),
-                          spreadRadius: -2.0,
-                          blurRadius: 5.0,
-                        ),
-                      ],
-                    ),
-                    child: Icon(
-                      Icons.play_circle_outline,
-                      color: Colors.white,
-                      size: 35.0,
-                    ),
-                  ),
-                ),
+                !_checking
+                    ? Column(
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.center,
+                            child: RegularText(
+                              text: 'Mau lihat score global?',
+                              dark: true,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.center,
+                            child: RegularText(
+                              text: 'Tonton iklan dulu yuk',
+                              dark: true,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          AdsScreen(
+                            adsMode: AdsMode.GlOBAL_SCORE,
+                            onReward: _handleReward,
+                            buttonShow: Container(
+                              padding: const EdgeInsets.all(15.0),
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: greenColor,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: textColor.withOpacity(0.5),
+                                    offset: const Offset(0.0, 0.0),
+                                  ),
+                                  BoxShadow(
+                                    color: textColor.withOpacity(0.5),
+                                    offset: const Offset(0.0, 0.1),
+                                    spreadRadius: -2.0,
+                                    blurRadius: 5.0,
+                                  ),
+                                ],
+                              ),
+                              child: Icon(
+                                Icons.play_circle_outline,
+                                color: Colors.white,
+                                size: 35.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    : const SpinKitDoubleBounce(color: greenColor),
               ],
             );
           } else if (state is GlobalHasScores) {
@@ -113,7 +151,7 @@ class _GlobalScoreState extends State<GlobalScore> {
               },
             );
           }
-          return const SpinKitRotatingPlain(color: greenColor);
+          return const SpinKitDoubleBounce(color: greenColor);
         },
       ),
     );
