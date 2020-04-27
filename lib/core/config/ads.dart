5e154c8efe670bff8ca39beed50f3aecee99e62d
Aris Jirat <arisjiratkurniawan@gmail.com>
2020-04-28 04:20:05 +0700

    ads static variable moved

--- lib/core/config/ads.dart
+++ lib/core/config/ads.dart
@@ -1,2 +1,3 @@
-const ADMOB_APP_ID = 'ca-app-pub-7945031394108607~2104533679';
+const ADMOB_APP_ID = 'ca-app-pub-7945031394108607';
+const ADMOB_PUBLISHER_ID = 'ca-app-pub-7945031394108607~2104533679';
 const List<String> ADMOB_KEYWORDS = <String>['Games', 'Kartu', 'Arab'];
